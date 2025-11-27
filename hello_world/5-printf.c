dfee7e5ec8b5c83b269439caa6561c1d2e1626ae
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-11-27 11:47:52 +0000

    init

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - Prints a sentence using printf
  * Return: 0 if successful
  */
- int main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
