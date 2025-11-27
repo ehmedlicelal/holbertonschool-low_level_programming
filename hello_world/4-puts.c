b65740472d1727be4875dddad58bbc38c606fdbb
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-11-27 11:26:07 +0000

    init

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - this is for printing "Programming is like building a multilingual puzzle"
+ * return (0) - if program runs with no errors it gives code 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
