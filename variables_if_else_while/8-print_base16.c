de01f248052afb259c43fed4f31a428e740af696
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 10:43:31 +0000

    C language

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char a;
+	
+	for (a = 0; a <= 9; a++)
+		putchar(a + '0');
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
