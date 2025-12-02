1acf3d6ace0f97267d555aea2b3bf681a8e542e4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 10:11:04 +0000

    C language

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints letters exept of q and e
+ *
  * Return: 0 if successful
  */
 int main(void){
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter == 'e' || letter == 'q')
