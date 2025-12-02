6dd04fb92afe53e12daf33aba6a35bd9761c2313
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 21:17:25 +0000

    init

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/** print_last_digit - function
+ * Return: returns last digit
+ * @c: parameter
+ */
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
+
