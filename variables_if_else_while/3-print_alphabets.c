232b5d4fdc807bad4f8274b1919e9288a565272a
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 09:54:17 +0000

    C language

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+        {
+                putchar(a);
+        }
+	putchar('\n');
+	return (0);
+}
