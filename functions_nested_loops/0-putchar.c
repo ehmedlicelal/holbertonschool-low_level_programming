360949c9a64d0fc7b135234c94aa9f4ba2d1febb
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 19:23:08 +0000

    init

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
+
 /**
  * main - entry point
  * Return: 0 if successful
@@ -7,6 +8,13 @@
 
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
