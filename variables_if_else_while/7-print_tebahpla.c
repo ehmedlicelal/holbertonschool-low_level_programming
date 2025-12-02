b9a94c0696e173098037d2c06b5f4e41109ad1d5
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 10:31:20 +0000

    C language

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: 0 if successful
+*/
+int main
+{
+  char letter;
+
+  for(letter = 'z'; letter >= 'a'; letter--)
+    putchar(letter);
+  putchar('\n');
+  return (0);
+}
