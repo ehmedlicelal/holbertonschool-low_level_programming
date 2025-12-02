81e00d2ac2b79891266ad6432e898af00b63678b
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-02 10:16:42 +0000

    C language

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
