63d3611aee6450500422d66912ffa72b530c511e
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-06 19:33:05 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle with #
+ * @size: parameter
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j > 0; j--)
+			_putchar(' ');
+		if (i <= size - 1)
+		{
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
