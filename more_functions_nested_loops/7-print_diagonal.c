50d24bf24c9c1a7a79cc11310d4432b8f41ad9c5
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-06 18:17:00 +0000

    init

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,13 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\');
+		_putchar('92');
 		_putchar('\n');
 	}
 	_putchar('\n');
