3e37179267d205b95e5f56906117a3641747c137
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-06 07:33:11 +0000

    init

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,13 +8,22 @@
 
 void times_table(void)
 {
-	int a,b;
+	int a,b,c,d,e;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 				{
-					_putchar((a * b) + '0');
+					e = a * b;
+					c = e / 10;
+					d = e % 10;
+					if (e > 9)
+					{
+						_putchar(c + '0');
+						_putchar(d + '0');
+					}
+					else
+						_putchar(e + '0');
 					if (b != 9)
 					{
 						_putchar(',');
