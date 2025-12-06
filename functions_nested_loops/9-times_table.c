49ff416c18e72f58b1b5d7b2a80d5aef61401bae
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-06 07:58:55 +0000

    init

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,28 +13,28 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
+		{
+			e = a * b;
+			c = e / 10;
+			d = e % 10;
+			if (e > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				if (b != 0)
 				{
-					e = a * b;
-					c = e / 10;
-					d = e % 10;
-					if (e > 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(c + '0');
-						_putchar(d + '0');
-					}
-					else
-					{
-						if (b != 0)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-						}
-						_putchar(e + '0');
-					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
+				_putchar(e + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
