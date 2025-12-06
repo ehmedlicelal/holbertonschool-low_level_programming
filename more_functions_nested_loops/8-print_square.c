2b6d41b3a28f2310c37c0380e2bcd4f45f33fdbe
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-06 18:41:34 +0000

    init

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints square with size of parameter
+ * @size: parameter
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
