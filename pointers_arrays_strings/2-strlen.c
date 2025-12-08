7b7419cdfe8c6386635fdbeebb10229def02cc7b
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-08 15:38:59 +0000

    init

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: parameter
+ */
+
+int _strlen(char *s)
+{
+	int i = 0, len = 0;
+
+	while (s[i++])
+	{
+		len++;
+	}
+	return (len);
+}
