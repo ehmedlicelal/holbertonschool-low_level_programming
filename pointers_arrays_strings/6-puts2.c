75776e441956b8d10f8f9f7193150e125a7a6ae4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-09 10:54:22 +0000

    init

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ * @str: parameter
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
