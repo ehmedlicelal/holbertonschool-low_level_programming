eb546acf794a5192c2cf092239661c0edc509b58
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-09 13:32:53 +0000

    arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_array
+ * @a: parameter
+ * @n: parameter
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (a[i] != n - 1)
+			printf("%d, ", a[i]);
+	printf("%d", a[n-1];
+}
