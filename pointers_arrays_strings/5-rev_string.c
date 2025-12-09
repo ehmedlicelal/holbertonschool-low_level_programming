536e90880e3c1a44957f9cc6241ee342d4d00805
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-09 10:05:12 +0000

    init

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, len = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		len++;
+	}
+	_putchar('\n');
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
