c8bf52a1e3bbd78d17fdda467804c0e548168d27
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-09 20:03:18 +0000

    arrays

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,15 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		if (i != n - 1)
 			printf("%d, ", a[i]);
 		else
 			printf ("%d", a[i]);
+		i++;
 	}
 	_putchar('\n');
 }
