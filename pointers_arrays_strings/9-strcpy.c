0d2968a459a6ce5e70b844c2992e16353ea04cf4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-10 17:34:50 +0000

    arrays

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: destination
+ * @src: source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest)
+}
