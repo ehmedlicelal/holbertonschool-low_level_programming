279833669582e6ace5373fa9bd824ce56d27f822
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-10 18:24:22 +0000

    arrays

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to int
+ * @s: parameter
+ *
+ * Return: returns integer converted from string
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do
+	{
+		if (*s == '-')
+			num *= -1;
+		else if (*s >= 0 && *s <= 0)
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	}
+	while (*s++);
+	return (num * sign);
+}
