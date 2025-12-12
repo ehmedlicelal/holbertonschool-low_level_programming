ebdf3f8e16ee927656bb2fb6ef407cde43167a86
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-12 18:37:56 +0000

    arrays

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string from src to dest
+ * @dest: destination string
+ * @src: source string
+ * 
+ * Return: returns dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && (*(src + i) != '\0'); i++)
+		*(dest + i) = *(src + i);
+	i += 1;
+	if (n - i >= 2)
+		for ( ; i < n; i++)
+			dest[i] = '\0';
+	return (dest);
+}
