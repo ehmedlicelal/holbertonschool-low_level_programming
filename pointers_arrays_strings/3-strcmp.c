e0f03b4a6427b5715f80ec0c0c26b2332b8fc77b
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-12 19:17:46 +0000

    arrays

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - entry point
+ * @s1: parameter
+ * @s2: parameter
+ * Return: returns result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, result;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			result += 0;
+		else
+			result += s1[i] - s2[i];
+	}
+	return (result);
+}
