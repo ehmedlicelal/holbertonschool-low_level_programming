a4182b1f55ef708757ee429d32dfd5e525ec26f4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-12 18:47:58 +0000

    arrays

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	if (src[0] == '\0')
-		return (dest);
 	for (i = 0; i < n && (*(src + i) != '\0'); i++)
 		*(dest + i) = *(src + i);
-	i += 1;
-	if (n - i >= 2)
-		for ( ; i < n; i++)
-			dest[i] = '\0';
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
 }
