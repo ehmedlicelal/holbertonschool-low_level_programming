61b20a4562510f8c4f20dbc1ed9bf6583c77446d
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-13 20:57:31 +0000

    arrays

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,13 +9,13 @@
 char *leet(char *s)
 {
 	int i = 0, j = 0;
-	char leet[8] = {'o', '.', 'e', 'a', '.', '.', '.', 't'};
+	char leet[8] = {'o', 'l', '.', 'e', 'a', '.', '.', 't'};
 
 	while (s[i] != '\0')
 	{
 		while (j <= 7)
 		{
-			if (s[i] == leet[j] || s[i] == leet[j] - 32)
+			if (s[i] == leet[j] || s[i] + 32 == leet[j])
 				s[i] = j + '0';
 			j++;
 		}
