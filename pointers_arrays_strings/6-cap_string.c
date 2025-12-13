b27dacbc4e93cae8fe479f3791e9ca90013e1e0a
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-13 20:12:53 +0000

    arrays

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if(!(s[i] >= 'a' && s[i] <= 'z'))
+		while (!(s[i] >= 'a' && s[i] <= 'z')
 			i++;
 		if (i == 0 ||
 			s[i - 1] == ' ' || s[i - 1] == '\t' ||
@@ -22,8 +22,7 @@ char *cap_string(char *s)
 			s[i - 1] == '"' || s[i - 1] == '(' ||
 			s[i - 1] == ')' || s[i - 1] == '{' ||
 			s[i - 1] == '}')
-			if(s[i] != ' ' || s[i] != '\n')
-				s[i] -= 32;
+			s[i] -= 32;
 		i++;
 	}
 	return (s);
