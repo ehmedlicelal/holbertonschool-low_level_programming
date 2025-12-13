baba1da94207633909921b6753341c8edca107a3
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-13 20:50:33 +0000

    arrays

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - entry point
+ * @s: string
+ * Return: returns string
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+	char leet[8] = ['o', '.', 'e', 'a', '.', '.', '.', 't']
+
+	while (s[i] != '\0')
+	{
+		while (j <= 7)
+		{
+			if (s[i] == leet[j] || s[i] == leet[j] - 32)
+				s[i] = j + '0';
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
