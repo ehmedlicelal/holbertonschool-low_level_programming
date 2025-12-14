8f9f239053303483b09202605ecafc99b9aa3ca4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-14 12:07:59 +0000

    arrays

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - returns first occurence of c in s
+ * @s: string
+ * @c: char
+ * Return: c if found, null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return s[i];
+		i++;
+	}
+	return (NULL);
+}
