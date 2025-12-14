391ae964e9d43154fc7d904053dcc7cfb7de5ac5
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-14 11:41:36 +0000

    arrays

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory adress of src to dest
+ * @dest: destination string
+ * @srf: source string
+ * @n: number
+ * Return: returns pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *a = &src;
+
+	for (i = 0; i < n; i++)
+		dest[i] = a[i];
+	return (dest);
+}
