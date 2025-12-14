69801c7923474903d72004c5f965e010f05a12ba
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-14 11:49:25 +0000

    arrays

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *b = &src;
-	char a = b;
 
 	for (i = 0; i < n; i++)
-		dest[i] = a[i];
+		dest[i] = src[i];
 	return (dest);
 }
