a6b7f0eab7fa251748514f16d581e2b83702c789
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-14 11:17:45 +0000

    arrays

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - sets all bytes off s to b
+ * @s: array
+ * @b: effectively making every char b
+ * @n: size of arra(in bytes)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
+}
