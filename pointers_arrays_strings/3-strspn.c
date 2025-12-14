e1f292e2fb14ff9a8c73481cefb9430c308f7808
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-14 14:30:11 +0000

    arrays

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - returns if s has accept
+ * @s: string
+ * @accept: initial segment of s
+ * Return: result
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int result;
+	int i = 0, j;
+
+	while (s[i])
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (result);
+		}
+		i++;
+	}
+	return (result);
+}
