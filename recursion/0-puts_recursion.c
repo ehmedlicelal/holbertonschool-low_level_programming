21fdf985eb5907db9260a02e083a03e156c5d4aa
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-15 14:29:22 +0000

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,11 +9,13 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 	
-	if (*(s + i) == '0')
+	if (*s == '0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*(s + i));
-	return (_puts_recursion(*(s + i + 1));
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
