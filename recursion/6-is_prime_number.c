3104cc771ae277ca7c790ea43135793f1e6316e4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-17 15:10:01 +0000

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
+int find_count_of_multipliers(int n, int count, int i)
+{
+	if (count > 2)
+		return (0);
+	else
+	{
+		if (n % i == 0)
+			count++;
+		return find_count_of_multipliers(n, count, i++);
+	}
+	return (1);
+}
+
 int is_prime_number(int n)
 {
-	
+	int count = 0;
+	int multiplier = 1;
+	if (n <= 3)
+		return (1);
+	else
+		return (find_count_of_multipliers(n, count, multiplier));
 }
