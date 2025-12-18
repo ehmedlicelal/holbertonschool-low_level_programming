2c79f94a448317e7168e7c43541a7e4e832aff22
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-18 12:47:09 +0000

    arg

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	printf("&s", arg[0]);
+	return (0);
+}
