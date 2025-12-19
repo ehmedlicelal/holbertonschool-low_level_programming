54033d0d0634aa9791265267a36716cbc1b46f9e
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-19 10:18:58 +0000

    args

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	if (argv[1].isdigit() && argv[2].isdigit())
+	{
+		printf("%d\n", argv[1] * argv[2]);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
