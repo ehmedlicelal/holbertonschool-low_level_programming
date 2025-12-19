4d526e07f5f4a1b8d2dd5cff687501b0c46a1a49
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-19 12:32:00 +0000

    args

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,20 +13,17 @@ int main(int argc, char **argv)
 {
 	int i, j, count = 0;
 
-	if (argc == 1)
-		puts("0\n");
 	for (i = 1; i < argc; i++)
 	{
-		j = atoi(argv[i]);
-		if (isdigit(j))
+		for (j = 0; argv[i][j]; j++)
 		{
-			count += j;
+			if (arg[i][j] < '0' || arg[i][j] > '9')
+			{
+				puts("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			puts("Error\n");
-			return (1);
-		}
+		count += arg[i];
 	}
 	printf("%d\n", count);
 	return (0);
