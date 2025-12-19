67253a5ec99f4d9312c44418621073de0df9d131
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-19 12:09:01 +0000

    args

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, count = 0;
+
+	if (argc == 1)
+		puts("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		j = atoi(argv[i]);
+		if (!isdigit(j))
+		{
+			puts("Error\n");
+			return (1);
+		}
+		else
+			count += j;
+	}
+	printf("%d\n", count);
+	return (0);
+}
