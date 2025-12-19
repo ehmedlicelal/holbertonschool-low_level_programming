275d297bce5999226c91f29c9afc51aea3f0db74
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-19 10:58:29 +0000

    args

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	arg1 = atoi(rgv[1]);
+	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[2]);
-	printf("%d\n", arg1 * arg2);:
+	printf("%d\n", arg1 * arg2);
 	return (0);
 }
