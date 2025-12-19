483a09310886584e8e2ab94d1b2d8fcee8dd320f
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-19 09:55:16 +0000

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	pirntf("%d\n",argc);
+	return (0);
+}
