3d76cd72490b4342b6ffb61a49d3e9f397cc8b2d
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-23 13:23:14 +0000

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *concated_str;
 	unsigned int i, len1 = 0, len2 = 0, len3 = 0;
 	
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		*s1 = "";
+	if (s2 == NULL)
+		*s2 = "";
 	while (s1[len1] != '\0')
 		len1++;
 	len3 += len1;
