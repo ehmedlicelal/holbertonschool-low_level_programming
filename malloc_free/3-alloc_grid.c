906319b849f3a4e25ed62686aa902a7cc0c4e9a4
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-23 18:34:20 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	*arr = malloc((width * height) * sizeof(int));
+	arr = malloc((width * height) * sizeof(int));
 	if (*arr == NULL)
 		return (NULL);
 
