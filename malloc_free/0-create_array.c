7e679c094ac1093516c080d96113a555c08218af
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-23 06:33:37 +0000

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 
 char *create_array(unsigned int size, char c)
@@ -5,6 +6,6 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	arr = malloc(sizeof(size));
-	arr[0] = char;
+	arr[0] = c;
 	return (arr);
 }
