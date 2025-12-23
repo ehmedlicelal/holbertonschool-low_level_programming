e6a50e9eaefc109ca618fc72285075cb07561cba
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-23 19:01:20 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,21 +11,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+      	int i, j;
 	int **arr;
 
-	if (width <= 0 || height <= 0)
+	if(width <= 0 || height <= 0)
 		return (NULL);
-	arr = (int **)malloc((width * height) * sizeof(int *));
-	if (*arr == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+	arr = (int **)malloc(height * sizeof(int *));
+	if (arr != NULL)
 	{
-		for (j = 0; j < width; j++)
+		for (i = 0; i < height; i++)
 		{
-			arr[i][j] = 0;
+			arr = (int *)malloc(width * sizeof(int));
+			if (arr[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					arr[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(arr[i]);
+					i--;
+				}
+				return (NULL);
+			}
 		}
+		return (arr);
 	}
-	return (arr);
+	else
+		return (NULL);
 }
