838bd4f8d95bdfab350972f1f4ac9310c0cd3899
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-31 07:23:18 +0000

    Structures, typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+void print_dog(struct dog *d)
+{
+	char null_element[] = "(nil)";
+
+	if (d != NULL)
+	{
+		if (d.name != NULL)
+			printf("Name: %s\n", d.name);
+		else
+			printf("Name: %s\n", null_element);
+		if (d.name != NULL)
+			printf("Name: %s\n", d.name);
+		else
+			printf("Name: %s\n", null_element);
+		if (d.name != NULL)
+			printf("Name: %s\n", d.name);
+   		else
+   			printf("Name: %s\n", null_element);
+	}
+}
