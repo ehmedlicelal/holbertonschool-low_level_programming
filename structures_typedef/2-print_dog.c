95ad4cf7fc2c1512ef70d33611415d0a35791d91
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-31 07:25:07 +0000

    Structures, typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,16 +7,16 @@ void print_dog(struct dog *d)
 
 	if (d != NULL)
 	{
-		if (d.name != NULL)
-			printf("Name: %s\n", d.name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: %s\n", null_element);
-		if (d.name != NULL)
-			printf("Name: %s\n", d.name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: %s\n", null_element);
-		if (d.name != NULL)
-			printf("Name: %s\n", d.name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
    		else
    			printf("Name: %s\n", null_element);
 	}
