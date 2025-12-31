7b2cd9e0a517ed4a121a165c3398a7be08f630f5
ehmedlicelal <ehmedlicelal1@gmail.com>
2025-12-31 07:34:26 +0000

    Structures, typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
-   		else
-   			printf("Owner: %s\n", null_element);
+		else
+			printf("Owner: %s\n", null_element);
 	}
 }
