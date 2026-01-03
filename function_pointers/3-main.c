041cfaa482f4a39ac0b7a7d63e3e5d8980b2b1e0
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-03 13:00:23 +0000

    Operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * 
+ * Return: 0 if successful
+ */
+
+int main(argc, **argv)
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+	
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((op == '/' && b == 0) || (op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+
+	return (0);
+}
