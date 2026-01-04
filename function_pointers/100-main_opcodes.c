88ec00cdeea3e652a50c2307b51e5f021440f25e
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-04 11:05:18 +0000

    OpCodes

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	int bytes, i;
+	char *append;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (argc < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	append = (char *)&main;
+	bytes = atoi(argv[1]);
+	
+	for (i = 0; i < bytes; i++)
+		printf("%02hhx ", append[i]);
+	_putchar("\n");
+	return (0);
+}
