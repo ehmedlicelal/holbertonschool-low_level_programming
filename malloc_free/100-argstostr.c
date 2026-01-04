3b959c2da95ec8788e44a3970496df426a3eb8d7
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-04 12:02:13 +0000

    Concat All Args

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,62 +1,39 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * len - length of string
- * @s: string
- * Return: len of string
+ * argstostr - concatenates all arguments
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to new string, or NULL on failure
  */
-
-int len(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * argstostr - concatenates all the args of the program
- * @ac: number of args
- * @av: array of args
- * Return: pointer to new string
- */
-
 char *argstostr(int ac, char **av)
 {
-	int len_a, i, j;
-	char **concated_arr;
+	int i, j, k = 0, total_len = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	concated_arr = (char **)malloc(ac * sizeof(char *));
-	if (concated_arr != NULL)
+
+	for (i = 0; i < ac; i++)
 	{
-		for (i = 0; i < ac; i++)
-		{
-			len_a = len(av[i + 1]);
-			concated_arr[i] = (char *)malloc(len_a * sizeof(char));
-			if (concated_arr[i] != NULL)
-			{
-				for (j = 0; j < len_a; j++)
-				{
-					concated_arr[i][j] = av[i + 1][j];
-				}
-			}
-			else
-			{
-				while (i >= 0)
-				{
-					free(concated_arr[i]);
-					i--;
-				}
-				free(concated_arr);
-				return (NULL);
-			}
-		}
-		return (concated_arr);
+		for (j = 0; av[i][j]; j++)
+			total_len++;
+		total_len++;
 	}
-	else
+
+	str = malloc(sizeof(char) * (total_len + 1));
+	if (str == NULL)
 		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			str[k++] = av[i][j];
+		str[k++] = '\n';
+	}
+
+	str[k] = '\0';
+
+	return (str);
 }
