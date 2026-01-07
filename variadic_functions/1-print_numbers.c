08dda03b8361c64838641e922163c61e86150116
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-07 09:41:54 +0000

    Print Numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n - 1; i++)
+		printf("%u%s", va_arg(nums, unsigned int), separator);
+	printf("%u\n", va_arg(nums, unsigned int));
+	va_end(nums);
+}
