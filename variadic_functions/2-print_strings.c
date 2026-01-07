0d532a42d82087b754a85470e331beb141de9cc3
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-07 14:52:36 +0000

    Print Strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -31,7 +31,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 		if (i < n - 1)
-		       printf("%s", separator);	
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(strings);
