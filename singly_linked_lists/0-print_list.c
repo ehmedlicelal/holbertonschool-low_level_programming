acb429f82dbcd238f7a56806a3308b9618016483
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-15 16:00:12 +0400

    Print List

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints list elements and count of them
+ * @h: list
+ * Return: returns count of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 1;
+
+	if (h == NULL)
+		return (0);
+	printf("[%d] %s", h->len, h->str);
+	if (h->next != NULL)
+		return print_list(h->next);
+	return (0);
+}
