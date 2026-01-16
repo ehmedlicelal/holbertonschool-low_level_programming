343eedf38eb184f20c47a56e48b3c6e75aca9a2f
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-16 13:19:13 +0400

    Number of Elements

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * list_len - number of elements in list
+ * @h: pointer to elements of type list_t
+ * Return: count of elements in list
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 1;
@@ -9,5 +15,9 @@ size_t list_len(const list_t *h)
 	if (h == NULL)
 		return (0);
 	while (h->next != NULL)
+	{
 		count++;
+		h = h->next;
+	}
+	return (count);
 }
