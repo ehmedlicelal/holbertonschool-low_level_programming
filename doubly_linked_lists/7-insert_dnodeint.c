0a8dce341e72add0a610a3938d4583d0d2bec745
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-18 14:53:15 +0000

    Insert at Index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,25 +6,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp, *new;
 	unsigned int counter;
 
+	if (*h == NULL && idx != 0)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
+	new->n = n, new->prev = new->next = NULL;
 	
 	counter = 0;
 	tmp = *h;
-	while (tmp)
+	while (tmp->next)
 	{
 		if (counter == idx)
 		{
 			new->prev = tmp->prev;
 			new->next = tmp;
 			tmp->prev = new;
-			return (tmp);
+			return (new);
 		}
 		tmp = tmp->next;
 	}
+	
+	if (tmp == NULL && counter == idx)
+	{
+		new->prev = tmp, tmp->next = new->prev;
+		return (new);
+	}
+	free(new);
 	return (NULL);
 }
