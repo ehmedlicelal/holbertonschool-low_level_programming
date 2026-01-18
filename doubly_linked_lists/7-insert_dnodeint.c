700db0c3766428c8cce7a8fad405fdf52e018607
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-18 14:29:59 +0000

    Insert at Index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = tmp->prev;
 			new->next = tmp;
 			tmp->prev = new;
+			return (tmp);
 		}
 		tmp = tmp->next;
 	}
