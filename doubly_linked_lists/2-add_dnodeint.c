4522ca9874625a4ec200698082ee58caf65709b2
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-01-18 07:41:49 +0000

    Add Node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - adds node to the head of the list
+ * @head: pointer to pointer to the head of the list
+ * @n: pointer of number of nodes
+ * Return: pointer to new list if successful
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
@@ -10,7 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new)
+	}
+	(*head)->prev = new;
 	new->next = *head;
-	new = *head;
+	*head = new;
 	return (new);
 }
